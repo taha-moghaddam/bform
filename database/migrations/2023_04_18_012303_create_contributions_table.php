e6c53ffc0b033ca6c381112dd8b88e4fd9f6731d
mtmoghaddam <m5taha5m@gmail.com>
2023-04-19 06:26:46 +0330

    feat: Add review to contributions-table

--- database/migrations/2023_04_18_012303_create_contributions_table.php
+++ database/migrations/2023_04_18_012303_create_contributions_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Bikaraan\BForm\Enums\ReviewStatus;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,6 +16,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id');
             $table->foreignId('form_id');
+            $table->enum('review_status', ReviewStatus::values())->default(ReviewStatus::PENDING->value);
+            $table->timestamp('reviewed_at')->nullable();
+            $table->text('review_comment')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
